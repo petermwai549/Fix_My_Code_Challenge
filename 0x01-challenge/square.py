a108c3c86c4b1fce7ff303d2b78fb08dc7deedd1
Peter Mwai <petermwai549@gmail.com>
2023-04-10 19:18:59 +0300

    Fix_My_Code_Challenge

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+A Module for modeling Square obj
+"""
+
+
+class square():
+    """ A class Square
+    """
+
+    def __init__(self, width, height):
+        """
+        initialize
+        """
+        self.width = width
+        self.height = height
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def PermiterOfMySquare(self):
+        """ Perimeter of the square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ String format of square """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    """
+    deriver code
+    """
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
